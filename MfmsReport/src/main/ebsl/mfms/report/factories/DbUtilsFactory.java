018131d88979bc4d1050e14723aac688d3c47bc4
mandaveph <mandaveph@C-7960.swd.hksarg>
2018-01-31 14:05:00 +0800

    initialization progressing

---
+++ MfmsReport/src/main/ebsl/mfms/report/factories/DbUtilsFactory.java
@@ -0,0 +1,37 @@
+package ebsl.mfms.report.factories;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ebsl.mfms.report.utils.DbUtils;
+import ebsl.mfms.report.utils.MySqlDbUtils;
+public class DbUtilsFactory {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private String className = this.getClass().getName();
+	private static DbUtils mySqlDbUtils;
+	private static DbUtils db2DbUtils;
+	private static DbUtils msSqlDbUtils;
+	
+	private DbUtilsFactory(){
+		
+	}
+	public static DbUtils getInstanceOfMySqlDbUtils() throws Exception{
+		if (mySqlDbUtils == null){
+			mySqlDbUtils = new MySqlDbUtils();
+		}
+		return mySqlDbUtils;
+	}
+//	public static DbUtils getInstanceOfDb2DbUtils() throws Exception{
+//		if (db2DbUtils == null){
+//			db2DbUtils = new Db2DbUtils();
+//		}
+//		return db2DbUtils;
+//	}
+//	
+//	public static DbUtils getInstanceOfMsSqlDbUtils() throws Exception{
+//		if (msSqlDbUtils == null){
+//			msSqlDbUtils = new MsSqlDbUtils();
+//		}
+//		return msSqlDbUtils;
+//	}
+}
