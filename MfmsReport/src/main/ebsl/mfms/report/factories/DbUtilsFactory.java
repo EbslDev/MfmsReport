978d596e0b55e2658379b73b32a090af14be2764
mandaveph <mandaveph@C-7960.swd.hksarg>
2018-02-02 14:35:04 +0800

    updated 20180202_1435

--- MfmsReport/src/main/ebsl/mfms/report/factories/DbUtilsFactory.java
+++ MfmsReport/src/main/ebsl/mfms/report/factories/DbUtilsFactory.java
@@ -6,16 +6,29 @@ import org.slf4j.LoggerFactory;
 import ebsl.mfms.report.utils.DbUtils;
 import ebsl.mfms.report.utils.MySqlDbUtils;
 public class DbUtilsFactory {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	private String className = this.getClass().getName();
+	private final static Logger logger = LoggerFactory.getLogger(getClassName());
 	private static DbUtils mySqlDbUtils;
 //	private static DbUtils db2DbUtils;
 //	private static DbUtils msSqlDbUtils;
-	
+	private static DbUtilsFactory dbUtilsFactory;
 	private DbUtilsFactory(){
 		
 	}
-	public static DbUtils getInstanceOfMySqlDbUtils() throws Exception{
+	public static String getClassName() {
+		String className = DbUtilsFactory.class.getName();
+		return className;
+	}
+	public static DbUtilsFactory getInstanceOfDbUtilsFactory() throws Exception {
+		try {
+			if (dbUtilsFactory == null) {
+				dbUtilsFactory = new DbUtilsFactory();
+			}
+		} catch (Exception e) {
+			logger.error(getClassName() + ".getInstanceOfDbUtilsFactory() - ", e);
+		}
+		return dbUtilsFactory;
+	}
+	public DbUtils getInstanceOfMySqlDbUtils() throws Exception{
 		if (mySqlDbUtils == null){
 			mySqlDbUtils = new MySqlDbUtils();
 		}
