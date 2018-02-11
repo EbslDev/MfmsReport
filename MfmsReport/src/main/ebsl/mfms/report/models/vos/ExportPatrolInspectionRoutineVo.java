40a7e27c821a7ca0959c17d8e6166e35f7d44fb4
keith <keith@VAIO>
2018-02-12 07:32:49 +0800

    Create Inspection Report VO

---
+++ MfmsReport/src/main/ebsl/mfms/report/models/vos/ExportPatrolInspectionRoutineVo.java
@@ -0,0 +1,51 @@
+package ebsl.mfms.report.models.vos;
+
+import java.util.Date;
+import java.util.List; 
+
+public class ExportPatrolInspectionRoutineVo extends VoBase{
+	
+	private Date patrolDate;
+	private String routeName;
+	private String locationName;
+	private String remark;
+	private List<String> photoPaths;
+	
+	
+	public Date getpPatrolDate() {
+		return patrolDate;
+	}
+	public void setPatrolDate(Date patrolDate) {
+		this.patrolDate= patrolDate;
+	}
+	public String getRouteName() {
+		return routeName;
+	}
+	public void setRouteName(String routeName) {
+		this.routeName= routeName;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark= remark;
+	}
+	
+	public List<String> getPhotoPaths() {
+		return photoPaths;
+	}
+	public void setLocationName(List<String> photoPaths) {
+		this.photoPaths = photoPaths;
+	}
+	
+	
+	
+}
